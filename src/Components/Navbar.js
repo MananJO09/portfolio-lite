5da941e2f1b688b01e61e91397df4f5fa80a39d7
manan_joshi <joshimanan72@gmail.com>
2023-11-02 23:41:15 +0530

    changes

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import "./style.css" ;
+
+const Navbar= () => {
+    return (
+        <div className="navbar">
+            <h1>Manandeep Joshi</h1>
+            <div className="nav">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Skills</a>
+                <a href="#">Projects</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
